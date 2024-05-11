458cf1aea5f7622e0de6548617655b782e05c0a7
william2208 <143356763+william2208@users.noreply.github.com>
2024-05-11 18:21:30 -0500

    Add files via upload
    esta es la presentación de los datos por medio de Python :)

---
+++ Tipo ingreso.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import psycopg2 as psy2
+import dash
+from dash import dcc,Input,Output,callback
+from dash import html
+import plotly.express as px
+from dash import dash_table as tbl
+import dash_bootstrap_components as dbc
+
+
+
+dbname = "Base argentina"
+user = "javier"
+password = "123456"
+host = "localhost"
+
+
+conn = psy2.connect(dbname =dbname, user = user, password = password, host = host)
+
+sql_query2 = "Select * from Tipo_ingreso;"
+
+dz =pd.read_sql(sql_query2, conn)
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.Div([
+        html.H1('A data-driven analysis of Argentinian wages and household economics in 2019'),
+        html.Img(src='complementos/cat.png')
+        ], className = 'banner'),
+    dbc.Container([
+        dbc.Label('Miembro'),
+        tbl.DataTable(
+        id='table',
+        columns=[{"id":i} for i in dz.head(10)],
+        data=dz.head(10).to_dict('records')
+        ),
+        dbc.Alert(id='tbl_out')
+    ]),
+])
+
+@callback(Output('tbl_out','children'),Input('tbl','active_cell'))
+def update_graphs(active_cell):
+    return str(active_cell) if active_cell else "Click the table"
+
+if __name__ == '__main__':
+    app.run(port=8085)
